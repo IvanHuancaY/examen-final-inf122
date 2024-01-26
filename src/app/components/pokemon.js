7826f6302c15b2a1fbfd47a25239c6dcdb532da5
IvanHuancaY <ivanyujra275@gmail.com>
2024-01-26 09:10:01 -0400

    26-01-2024

---
+++ src/app/components/pokemon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import style from './pokemon.module.css'
+import { useEffect, useState } from "react";
+
+function Pokemon(){
+    const [pokemon, setPokemon]=useState("vercel.sg");
+    
+    return(
+
+    );
+}
+
+export default Pokemon;
